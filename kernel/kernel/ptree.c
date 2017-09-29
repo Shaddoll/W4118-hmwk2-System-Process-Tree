be4b350516e69dcf2c9e5d7d2d82e6a64f20b36c
El Shaddai <chenzijian13@gmail.com>
2017-09-29 01:32:35 -0400

    fix sibling bugs

--- kernel/kernel/ptree.c
+++ kernel/kernel/ptree.c
@@ -5,7 +5,6 @@
 #include <linux/cred.h>
 #include <trace/events/sched.h>
 #include <asm-generic/errno-base.h>
-//#include <asm-generic/uaccess.h>
 
 int last_child(struct task_struct *p);
 void insert(struct task_struct *t, struct prinfo __user *buf, int pos);
@@ -18,44 +17,32 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 
 int last_child(struct task_struct *p)
 {
-	printk("=======enter last_child=========\n");
-	struct task_struct *parent = p->parent;
+	struct task_struct *parent = p->real_parent;
 	struct task_struct *p1 = list_entry(p->sibling.next, struct task_struct, sibling);
-	struct task_struct *p2 = list_entry(parent->children.next, struct task_struct, children);
-	printk("=======exit last_child=========\n");
-	return p1 == p2;
+	struct task_struct *p2 = list_entry(parent->children.prev, struct task_struct, sibling);
+	printk("last_child: %d, %d, %d", parent->pid, p1->pid, p2->pid);
+	return p == p2;
 }
 
 void insert(struct task_struct *t, struct prinfo __user *buf, int pos)
 {
 	int i;
-	printk("=======enter insert=========\n");
+	int x;
 	struct prinfo result = {0};
-	printk("=======insert 1========= comm: %s pid: %d\n", t->comm, t->pid);
 	result.pid = t->pid;
-	printk("=======insert 2=========\n");
-	//if (t->real_parent == NULL)
-	//result.parent_pid = t->real_parent->pid;
-	printk("=======insert 3=========\n");
-	result.next_sibling_pid = container_of(t->sibling.next, struct task_struct, sibling)->pid;
-	printk("=======insert 4=========\n");
+	result.parent_pid = t->real_parent->pid;
+	result.next_sibling_pid = list_entry(t->sibling.next, struct task_struct, sibling)->pid;
 	result.state = t->state;
-	printk("=======insert 5=========\n");
 	result.uid = current_uid();
-	printk("=======insert 6=========\n");
 	result.first_child_pid = 0;
-	printk("=======insert 7=========\n");
-	if (!list_empty(&t->children))
+	if (!list_empty(&t->children)) {
 		result.first_child_pid = list_entry(t->children.next, struct task_struct, sibling)->pid;
-	printk("=======insert 8=========\n");
+		x = list_entry(t->children.prev, struct task_struct, sibling)->pid;
+	}
 	for(i = 0; i < 16; i++)
 		result.comm[i] = t->comm[i];	
-	printk("=======insert 9=========\n");
 	copy_to_user(buf + pos, &result, sizeof(struct prinfo));
-	printk("========%s,%d,%ld,%d,%d,%d,%ld\n", result.comm, result.pid, result.state,
-		result.parent_pid, result.first_child_pid, result.next_sibling_pid, result.uid);
-	printk("=======exit insert=========\n");
-	//buf[pos] = result;
+	printk("========%s,%d,%ld,%d,%d,%d,%ld, %d\n", result.comm, result.pid, result.state,result.parent_pid, result.first_child_pid, result.next_sibling_pid, result.uid, x);
 }
 
 int do_ptree(struct prinfo __user *buf, int __user *nr)
@@ -75,55 +62,51 @@ int do_ptree(struct prinfo __user *buf, int __user *nr)
 	//	return -EFAULT;
 	
 	read_lock(&tasklist_lock);
-	for_each_process(p)
+	for_each_process(p) {
+		printk("%d\n", p->pid);
 		++count;
+	}
 	st = kcalloc(knr, sizeof(struct task_struct), GFP_KERNEL);
 
 	p = &init_task;
 	st[size++] = p;
+	printk("push: %d, %d\n", p->pid, p->group_leader->pid);
 	while (size > 0) {
-		printk("===============WHILE===============\n %d\n", size);
 		p = st[size - 1];
 		insert(p, buf, n_copy++);
 		
-		
-		printk("=======gao 1 start=========\n");
 		if (n_copy == knr)
 			break;
 		if (list_empty(&p->children)) {
 			// if no children, search siblings
 			--size; // pop up the current task from the stack
-			printk("=======gao 2 start=========\n");
+			printk("pop: %d\n", p->pid);
 			while (size > 0 && last_child(p)) {
 				// if it is the last child of its parent, pop it
 
 				p = st[size - 1];
 				--size;
+				printk("pop: %d\n", p->pid);
 			}
-			printk("=======gao 2 end=========\n");
-			printk("=======gao 3 start=========\n");
 			if (size > 0) {
 				// if it is not the last child of its parent
 
 				p = list_entry(p->sibling.next, struct task_struct, sibling);
 				st[size++] = p;
+				printk("push: %d, %d\n", p->pid, p->group_leader->pid);
 			}
-			printk("=======gao 3 end=========\n");
 			
 		} else {
 			// if has some children, push the children in stack
 
-			printk("=======gao 4 start=========\n");
 			p = list_entry(p->children.next, struct task_struct, sibling);
 			st[size++] = p;
-			printk("=======gao 4 end=========\n");
+			printk("push: %d, %d\n", p->pid, p->group_leader->pid);
 		}
-		printk("=======gao 1 end=========\n");
 
 	}
 	read_unlock(&tasklist_lock);
 	kfree(st);
 	//*nr = n_copy;
-	printk("===============RETURN===============");
 	return count;
 }
