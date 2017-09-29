10b76a08af35b45eabd7848deb2e298359ab4db1
HAOCI_ZHANG <zhanghaoci@gmail.com>
2017-09-29 10:32:17 -0400

    test.c

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -21,10 +21,10 @@ int main()
 	printf("\n===============\nTotal entries: %d\n===============\n", ret);
 	printf("\n===============\nNr: %d\n===============\n", nr);
 	
-	//dfs_print(buf, nr, 0, 0);
-	for (i = 0; i < nr; i++) {
-		print_process(buf[i], 0);
-	}
+	dfs_print(buf, nr, 0, 0);
+	//for (i = 0; i < nr; i++) {
+	//	print_process(buf[i], 0);
+	//}
 	
 	return 0;
 }
