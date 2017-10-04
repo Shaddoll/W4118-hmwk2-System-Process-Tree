b7f404b75b90bcd79867e1ba6a870412e8fa9ad6
Xianda Gao <xg2280@columbia.edu>
2017-10-04 10:43:08 -0400

    a

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -5,8 +5,32 @@
 #include <sys/syscall.h>
 #include "../kernel/include/linux/prinfo.h"
 
-int dfs_print(struct prinfo *tree, const int size, int cur, int indent);
-void print_process(const struct prinfo pr, int indent);
+
+void print_process(const struct prinfo pr, int indent) {
+	int i;
+
+	for (i = 0; i < indent; i ++)
+		printf("\t");
+	printf("%s,%d,%ld,%d,%d,%d,%ld\n", pr.comm, pr.pid, pr.state,
+		pr.parent_pid, (int)pr.first_child_pid, pr.next_sibling_pid, pr.uid);
+}
+
+
+int dfs_print(struct prinfo *tree, const int size, int cur, int indent) {
+	int cur_pid;
+
+	if (cur >= size)
+		return cur;
+
+	print_process(tree[cur], indent);
+
+	cur_pid = tree[cur++].pid;
+	while (cur < size && tree[cur].parent_pid == cur_pid) {
+		cur = dfs_print(tree, size, cur, indent + 1);
+	}
+
+	return cur;
+}
 
 
 int main()
@@ -32,31 +56,4 @@ int main()
 	
 	return 0;
 }
-
-int dfs_print(struct prinfo *tree, const int size, int cur, int indent) {
-	int cur_pid;
-
-	if (cur >= size)
-		return cur;
-
-	print_process(tree[cur], indent);
-
-	cur_pid = tree[cur++].pid;
-	while (cur < size && tree[cur].parent_pid == cur_pid) {
-		cur = dfs_print(tree, size, cur, indent + 1);
-	}
-
-	return cur;
-}
-
-void print_process(const struct prinfo pr, int indent) {
-	int i;
-
-	for (i = 0; i < indent; i ++)
-		printf("\t");
-	printf("%s,%d,%ld,%d,%d,%d,%ld\n", pr.comm, pr.pid, pr.state,
-		pr.parent_pid, (int)pr.first_child_pid, pr.next_sibling_pid, pr.uid);
-}
-
-
 
